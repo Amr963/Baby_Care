aef34b22f6c3f359e9ac8e36584a8891cc8d0cb5
farah makkawi <farahmk583@gmail.com>
2024-03-13 02:27:17 +0300

    s

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('amr', function () {
-    return view('welcome');
-});
-Route::get('amr23', function () {
-    return view('welcome');
-});
