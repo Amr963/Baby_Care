9c1d52e224c2f1861b4d290649ccf2236a0c17a3
unknown <AmrDalati.963@gmail.com>
2024-03-13 01:48:41 +0300

    test 15

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-
+Route::get('amr', function () {
+    return view('welcome');
+});
 Route::get('farah', function () {
     return view('welcome');
 });
