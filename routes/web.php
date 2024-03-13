3152e8f1e3875ef15e798458c50ebc313d7654ae
Amr963 <AmrDalati.963@gmail.com>
2024-03-13 16:46:41 +0300

    test2505

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test1', function () {
+    return view('welcome1');
+});
+
+Route::get('/test2', function () {
+    return view('welcome2');
+});
