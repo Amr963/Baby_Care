601484d54bcc962aba39f2beb4584e4d075f60d4
farah makkawi <farahmk583@gmail.com>
2024-03-20 01:10:49 +0300

    m

--- resources/views/vaccine/displayRightVaccine.blade.php
+++ resources/views/vaccine/displayRightVaccine.blade.php
@@ -44,18 +44,34 @@
             float: left;
             margin-right: 20px;
         }
+
+        .video {
+            float: left;
+            margin-right: 20px;
+            margin-block-start: -250px
+        }
     </style>
 </head>
 <body>
     <div class="container">
         @foreach ($recommended_age as $age)
             <h2>
-                <img class="image" src="{{Storage::url($age->image_path_vaccines)}}" alt="صورة" width="100" height="100">
+                <img class="image" src="{{Storage::url($age->image_path_vaccines)}}" alt="صورة" width="170" height="150">
                 اللقاح المناسب لطفلك بهذا العمر : <span class="highlight">{{$age->name}}</span>
                 <br>
             </h2>
 
-            <b>حول اللقاح  <p>{{$age->description}}</p></b>
+            
+                <b>حول اللقاح &rarrhk; <p>{{$age->description}}</p></b><br><br>
+                <b>لماذا يتم اخذ اللقاح &rarrhk;<p>{{$age->indication}}</p></b><br><br>
+                <b>ارشادات &rarrhk;<p>{{$age->guidelines}}</p></b><br><br>
+                <b class="location">مكان حقن اللقاح &rarrhk;<p>{{$age->injection_location}}</p>
+                <div class="video">
+                    <video width="250" height="160" controls>
+                        <source src="{{Storage::url($age->short_video_path_vaccines)}}" type="video/mp4">
+                    </video>
+                </b><br><br>
+            </div>
         @endforeach
     </div>
 </body>
